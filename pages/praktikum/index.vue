8d590142b9f89bebc73c8e0e732b7635dffdc16c
Adiguna7 <sevensag@gmail.com>
2020-10-19 04:48:06 +0700

    add praktikum page

---
+++ pages/praktikum/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container id="hero" fluid>
+    <v-row
+      class="d-flex align-center justify-center flex-column"
+      style="height: 100%"
+    >
+      <vue-typed-js
+        :strings="['_tender']"
+        :typeSpeed="100"
+        :startDelay="100"
+        :backSpeed="100"
+        :loop="true"
+      >
+        <div class="white--text text-sm-h4 text-h5 text-center">
+          <div>
+            Fi<a
+              class="white--text"
+              style="text-decoration: none; cursor: none"
+              href="https://line.me/ti/p/o8C_pypi95"
+              >n</a
+            ><a
+              class="white--text"
+              style="text-decoration: none; cursor: none"
+              href="https://line.me/ti/p/o8C_pypi95"
+              >d</a
+            >
+            M<a
+              class="white--text"
+              style="text-decoration: none; cursor: none"
+              href="https://line.me/ti/p/o8C_pypi95"
+              >e</a
+            >
+          </div>
+          <span class="typing" style="color: #e31b6d"></span>
+        </div>
+      </vue-typed-js>
+    </v-row>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+#hero {
+  height: 100vh !important;
+  width: 100vw !important;
+  background-color: #252934;
+}
+</style>
